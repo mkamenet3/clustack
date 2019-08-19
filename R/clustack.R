93a6758a21949e7a0f4b28eeaedb772395676ded
mkamenet3 <maria.kamenetsky@gmail.com>
2019-08-19 08:43:45 -0500

    oops - had forgotten that everything outside the cluster is assumed to be an RR of 1

--- R/clustack.R
+++ R/clustack.R
@@ -13,57 +13,66 @@ colorsmk <- function (x) {
     rgb(y[, 1], y[, 2], y[, 3], maxColorValue = 255)
 }
 
-plotmap <- function(res, pdfname=NULL){
+plotmap <- function(res, pdfname=NULL, genpdf=TRUE){
     #cluster_ix <- redblue(log(2 *  pmax(1/2, pmin(res, 2)))/log(4))
-    colmax <- 4;cluster_ix <- colorsmk(log(colmax *  pmax(1/colmax, pmin(res, colmax)))/log(colmax^2))
+    cluster_ix <- redblue(log(2 *  pmax(1/2, pmin(res, 2)))/log(4))
+    #colmax <- 4;cluster_ix <- colorsmk(log(colmax *  pmax(1/colmax, pmin(res, colmax)))/log(colmax^2))
     
     colors_0 <- matrix(cluster_ix, ncol=5, byrow = FALSE)
-    #pdf(pdfname, height=11, width=10)
+    if(genpdf==TRUE){
+        pdf(pdfname, height=11, width=10)    
+    }
+    
     
     par(fig=c(0,.2,.6,1), mar=c(.5,0.5,0.5,0))
     plot(japan.poly2,type='n',asp=1,axes=F,xlab='',ylab='')
     polygon(japan.poly2,col=colors_0[,1] ,border=F)
     segments(japan.prefect2$x1,japan.prefect2$y1,japan.prefect2$x2,japan.prefect2$y2)
-    text(355,4120,paste0("Period 1"),cex=1.00)
+    #text(355,4120,paste0("Period 1"),cex=1.00)
     
     #P2
     par(fig=c(0.2,.4,.6,1), mar=c(.5,0.5,0.5,0), new=T)
     plot(japan.poly2,type='n',asp=1,axes=F,xlab='',ylab='')
     polygon(japan.poly2,col=colors_0[,2] ,border=F)
     segments(japan.prefect2$x1,japan.prefect2$y1,japan.prefect2$x2,japan.prefect2$y2)
-    text(355,4120,paste0("Period 2"),cex=1.00)
+    #text(355,4120,paste0("Period 2"),cex=1.00)
     
     #P3
     par(fig=c(0.4,.6,.6,1), mar=c(.5,0.5,0.5,0), new=T)
     plot(japan.poly2,type='n',asp=1,axes=F,xlab='',ylab='')
     polygon(japan.poly2,col=colors_0[,3] ,border=F)
     segments(japan.prefect2$x1,japan.prefect2$y1,japan.prefect2$x2,japan.prefect2$y2)
-    text(355,4120,paste0("Period 3"),cex=1.00)
+    #text(355,4120,paste0("Period 3"),cex=1.00)
     
     #P4
     par(fig=c(0.6,.8,.6,1), mar=c(.5,0.5,0.5,0), new=T)
     plot(japan.poly2,type='n',asp=1,axes=F,xlab='',ylab='')
     polygon(japan.poly2,col=colors_0[,4] ,border=F)
     segments(japan.prefect2$x1,japan.prefect2$y1,japan.prefect2$x2,japan.prefect2$y2)
-    text(355,4120,paste0("Period 4"),cex=1.00)
+    #text(355,4120,paste0("Period 4"),cex=1.00)
     
     #P5
     par(fig=c(0.8,1,.6,1), mar=c(.5,0.5,0.5,0), new=T)
     plot(japan.poly2,type='n',asp=1,axes=F,xlab='',ylab='')
     polygon(japan.poly2,col=colors_0[,5] ,border=F)
     segments(japan.prefect2$x1,japan.prefect2$y1,japan.prefect2$x2,japan.prefect2$y2)
-    text(355,4120,paste0("Period 5"),cex=1.00)
+    #text(355,4120,paste0("Period 5"),cex=1.00)
     
     #legend
     par(fig=c(.35,.75,0,.1), new=T)
     plot(1, xlim=c(0.6,1.5), ylim=c(0.2,1), axes=F, type='n',  xlab="", ylab="")
     rect(seq(.6,1.4,length=50)[-50],.5,seq(.65,1.4,length=50)[-1],.62,col=redblue(0:50/50),border=F)
+    text(seq(.6,1.4,length=5),rep(.45,5),seq(0,2,length.out=5),srt=330,adj=0)
+    
+    #rect(seq(.6,1.4,length=50)[-50],.5,seq(.65,1.4,length=50)[-1],.62,col=redblue(0:50/50),border=F)
     #rect(seq(.6,1.4,length=50)[-50],.5,seq(.65,1.4,length=50)[-1],.62,col=pal(50),border=F)
     
     #rect(seq(.6,1.4,length=50)[-50],.5,seq(.65,1.4,length=50)[-1],.62,col=colorsmk(0:50/50),border=F)
-    text(seq(.6,1.4,length=5),rep(.45,5),seq(0,4,length.out=5),srt=330,adj=0)
+    #text(seq(.6,1.4,length=5),rep(.45,5),seq(0,4,length.out=5),srt=330,adj=0)
+    if(genpdf==TRUE){
+        dev.off()    
+    }
     
-    #dev.off()
 }
 
 # likweights2 <- function(liki){
@@ -92,57 +101,46 @@ poisLik <-function(Ex, Yx, sparsemat){
     #return likelihood for each potential cluster
     outExp <- sparsemat%*%Ex
     outObs <- sparsemat%*%Yx
-    #lambdahat <- ifelse(outObs==0, 0, outObs/outExp)
+    #calc Lambda
     lambdahat <- outObs/outExp
-    #outlogLik <- outObs * log(lambdahat * outExp) - (lambdahat * outExp)
-    #outlogLik <- outObs * log((outObs/outExp) * outExp) - ((outObs/outExp)* outExp)
-    
-    
-    
-    
-    
-    Lik <- (((outObs/outExp)/(sum(outObs)/sum(outExp)))^(outObs))*(((sum(outObs) - outObs)/(sum(outExp)-outExp))/(sum(outObs)/sum(outExp)))^(sum(outObs)-outObs)
+    Lambda <- as.vector(lambdahat)*sparsemat #big Lambda matrix
+    Lambda_dense <- as.matrix(Lambda)
+    Lambda_dense[Lambda_dense == 0] <- 1
+    #Get scaled likelihood
+    Lik <- ((outObs/outExp)/(sum(outObs)/sum(outExp)))^outObs
     outlogLik <- log(Lik)
-    
-    #outlogLik <- outObs * ifelse(is.infinite(log(lambdahat * outExp)),0,log(lambdahat * outExp)) - (lambdahat * outExp) #old 2019-07-22
-    # if(any(is.na(outlogLik))){
-    #     ix <- which(is.na(outlogLik))
-    #     #print(ix)
-    #     #outlogLik[ix] <- (lambdahat[ix]*outExp[ix])
-    #     outlogLik[ix] <- (outObs[ix]/outExp[ix])*outExp[ix]
-    # }
     outlogLik_scaled <- outlogLik-max(outlogLik)
-    outLik <- exp(outlogLik_scaled)
-    return(list(outLik=outLik,
-                lambdahat=lambdahat))
+    Lik <- exp(outlogLik_scaled)
+    return(list(Lik=Lik,
+                Lambda_dense=Lambda_dense))
 }
 
-bylocation <- function(outLik, sparsemat, locLambdas, Lambda,maxclust){
-    for(i in 1:maxclust){
+bylocation <- function(Lik, sparsemat, locLambdas, Lambda_dense,maxclust){
+    wi <- likweights(Lik) #tiny weights
+    for (i in 1:maxclust){
         message(paste0("Searching for cluster ",i))
-        #01) Get weight for each PC
-        wi <- likweights(outLik)
-        #wi <- likweights2(outLik)
-        #1) Find LOCATION with highest weight
-        lwi <- t(wi)%*%sparsemat#Lambda
-        maxlwi <- which.max(lwi@x) #552
-        message(paste0("Location identified: ",(maxlwi)))
-        #2) Find All PC's that Cover the max location
-        locmax <- rep(0,numCenters*Time); locmax[maxlwi] <-1
-        findoverlap <- sparsemat%*%locmax
-        ix <- which(findoverlap!=0)
-        wxi <- likweights(outLik[ix])
-        #wxi <- likweights2(outLik[ix])
-        wi[ix] <- wxi
-        #4) Find weighted Lambda by locations
-        locLambdas[[i]] <- t(wi)%*%Lambda #locLambdas[[i]] <- lwi
-        #5) Set Likelihood in cluster i to zero
-        outLik[ix] <- 0
+        #find location wiht largest weight
+        wi_loc <-t(wi)%*%sparsemat
+        maxloc <- which.max(as.vector(wi_loc))
+        message(paste0("Location identified: ",(maxloc)))
+        #find all potential clusters that overlap that location
+        locmax <- rep(0,numCenters*Time); locmax[maxloc] <-1; locmax <- matrix(locmax,ncol=1)
+        pclocmax <- as.vector(t(locmax)%*%t(sparsemat))
+        #partition weights vector st for pclocmax=1, those weights sum to 1
+        Lik[which(pclocmax!=0)] <-1
+        #reweight whats in the cluster so that sums to 1
+        wi[which(pclocmax!=0)] <- likweights(Lik[which(pclocmax!=0)])
         
+        out <- t(wi)%*%Lambda_dense
+        locLambdas[[i]] <- out 
+        #e) Set Lik for overlapping locations to zero
+        Lik[which(pclocmax!=0)] <-0
+        #f) Recalculate scaled likelihoods
+        wi <- likweights(Lik)
     }
-    return(locLambdas = locLambdas)
+    return(locLambdas=locLambdas)
 }
-
+    
 detectclusters <- function(sparseMAT, Ex, Yx,numCenters,Time, maxclust,bylocation=TRUE){
     #sparsemat large sparsematrix where rows are potential clusters and columns are space-time locations
     #Ex expected counts
--- R/clustack_RUN.R
+++ R/clustack_RUN.R
@@ -15,7 +15,8 @@ japan.prefect2 <- dframe.prefect2[,2:5]
 #set global
 rMax <- 20 
 Time <- 5
-maxclust <- 5
+maxclust <- 10
+locLambdas <- vector("list", maxclust)
 #create set of potential clusters based on distances
 potentialclusters <- clusso::clusters2df(x,y,rMax, utm = TRUE, length(x))
 n <- length(x)
@@ -32,153 +33,74 @@ sparseMAT <- spacetimeMat(potentialclusters, numCenters, Time)
 #res_loc <- detectclusters(sparseMAT, Ex, Yx,numCenters,Time, maxclust=4, bylocation=TRUE)
 sparsemat <- Matrix::t(sparseMAT) #66870x1040
 out <- poisLik(Ex, Yx, sparsemat)
-outLik <- out$outLik
-lambdahat <- out$lambdahat
-Lambda <- as.vector(lambdahat)*sparsemat #big Lambda matrix
-locLambdas <- vector("list", maxclust)
+Lik <- out$Lik
+Lambda_dense <- out$Lambda_dense
 
-res <- bylocation(outLik, sparsemat, locLambdas, Lambda, maxclust)
+res <- bylocation(Lik, sparsemat, locLambdas, Lambda_dense, maxclust)
 
 ###################################################
 #PLOTTING
-lapply(res, function(x){plotmap((x))})
-lapply(res_loc, function(x){plotmap(x)})
+lapply(res, function(x){plotmap((x), genpdf = FALSE)})
+lapply(res, function(x){summary((x@x))})
+#lapply(res_loc, function(x){plotmap(x)})
 
-#Saving plots for each maxclust
-vec <- 1:maxclust
-lapply(res, function(x) summary(x@x))
-lapply(1:maxclust, function(i){
-    name <- paste0("maxclust15_byloc_2019_10_7_nclust", vec[i], ".pdf")
-    plotmap(res[[i]], pdfname = name)
-})
-
-
-########################################
-#LONG FORM Update 7-9-2019
-########################################
-outExp <- sparsemat%*%Ex
-outObs <- sparsemat%*%Yx;outObs[outObs==0] <- 0.00001 #TOFIX
-lambdahat<- outObs/outExp#log(outObs/outExp)
-
-outLambda <- ifelse(outObs==0, 0, outObs/outExp)
-#outlogLik <- log(outObs*outLambda + (sum(outObs)-outObs)*log((sum(outObs)-outObs)/(sum(outExp)-outExp)))
-#outlogLik <- log(outExp*outLambda + (sum(outObs)-outObs)*log((sum(outObs)-outObs)/(sum(outExp)-outExp)))
+# #Saving plots for each maxclust
+# vec <- 1:maxclust
+# #lapply(res, function(x) summary(x@x))
+# lapply(1:maxclust, function(i){
+#     name <- paste0("maxclust15_byloc_2019_13_8_nclust", vec[i], ".pdf")
+#     plotmap(res[[i]], pdfname = name)
+# })
 
 
-outlogLik <- outObs * log(outLambda * outExp) - (outLambda * outExp)
-#outlogLik <- outObs * ifelse(is.na(log(outLambda * outExp)),0,log(outLambda * outExp)) - (outLambda * outExp)
-
-if(any(is.na(outlogLik))){
-    ix <- which(is.na(outlogLik))
-    outlogLik[ix] <- (outLambda[ix]*outExp[ix])
-}
-
-#outlogLik <- log(outObs*log(outObs/outExp) + (sum(outObs)-outObs)*log((sum(outObs)-outObs)/(sum(outExp)-outExp)))
-#outlogLik <- log(outObs*outLambda + (sum(outObs)-outObs)*log((sum(outObs)-outObs)/(sum(outExp)-outExp)))
-outlogLik_scaled <- outlogLik-max(outlogLik)
-outLik <- exp(outlogLik_scaled)
-Lambda <- outLambda*sparsemat
-# Lambda <- as.vector(lambdahat)*sparsemat #t(lambdahat)%*%sparsemat #1x1040 (lambda for each location)
-# outlogLik <- log(outObs*log(lambdahat) + (sum(outObs)-outObs)*log((sum(outObs)-outObs)/(sum(outExp)-outExp)))
-# outlogLik_scaled <- outlogLik-max(outlogLik)
-# outLik <- exp(outlogLik_scaled)
-wi <- likweights2(outLik) #66870x1
-
-lwi <- t(wi)%*%Lambda
-maxlwi <- which.max(lwi@x)
-message(paste0("Location identified: ",(maxlwi)))
-locmax <- rep(0,numCenters*Time); locmax[maxlwi] <-1
-findoverlap <- sparsemat%*%locmax
-ix <- which(findoverlap!=0)
-wxi <- likweights2(outLik[ix])
-wi[ix] <- wxi
-#tomap <- exp(t(wi)%*%Lambda)
-tomap <- exp(t(wi)%*%Lambda)
-
-
-#ixmap <- which(ifelse(findoverlap!=0,1,0)%*%sparsemat!=0)
-#locLambdas[ixmap] <- sum(Xi*outLambda[ix])
-
-
-#########
-lwi <- t(wi)%*%sparsemat
-maxlwi <- which.max(lwi@x) #552
-message(paste0("Location identified: ",(maxlwi)))
-#2) Find All PC's that Cover the max location
-locmax <- rep(0,numCenters*Time); locmax[maxlwi] <-1
-findoverlap <- sparsemat%*%locmax
-ix <- which(findoverlap!=0)
-wxi <- likweights(outLik[ix])
-sum(wxi)
-
-plotmap(log(tomap))
-
-
-########################################
-#LONG FORM Update 7-16-2019
-#this works but lik function is the long one
-########################################
-maxclust <- 4
-locLambdas <- vector("list", maxclust)
-
-outExp <- sparsemat%*%Ex #XE
-outObs <- sparsemat%*%Yx #XY
-lambdahat <- outObs/outExp
-Lambda <- as.vector(lambdahat)*sparsemat
-#calc Lik
-#Maybe I need to standardize the expected rates
-
-
-
-#outlogLik <- outObs * log(lambdahat*outExp) - (lambdahat*outExp)
-
-#outlogLik <- outObs * log(1*outExp) - (1*outExp)
-#outlogLik <- outObs*(log(outExp) + ifelse(is.infinite(log(lambdahat)),0,log(lambdahat))) - (lambdahat*outExp)
-
-outlogLik <- log(outObs*log(lambdahat)+((sum(outObs)-outObs)*1))
-
-#
-#outlogLik <- log(outObs*outLambda + (sum(outObs)-outObs)*log((sum(outObs)-outObs)/(sum(outExp)-outExp)))
-if(any(is.na(outlogLik))){
-    ix <- which(is.na(outlogLik))
-    outlogLik[ix] <- (lambdahat[ix]*outExp[ix])
-} #568 zeros after this in log lik
-outlogLik_scaled <- outlogLik - max(outlogLik)
-Lik <- exp(outlogLik_scaled)
-
-#calculate Lik directly
-#mu <- outExp*lambdahat
-#test <- mu^outObs*exp(-mu)
-#loglike <- dpoisson(outObs, lambdahat, outExp)
-
-
-
-for(i in 1:maxclust){
-    wi <- likweights(Lik)
-    lwi <- t(wi)%*%sparsemat
-    maxlwi <- which.max(lwi@x) #552
-    message(paste0("Location identified: ",(maxlwi)))
-    
-    locmax <- rep(0,numCenters*Time); locmax[maxlwi] <-1
-    findoverlap <- sparsemat%*%locmax
-    ix <- which(findoverlap!=0) #all PCs that overlap
-    
-    
-    #upweight
-    Lik[ix] <- 1
-    wxi <- likweights(Lik[ix])
-    print(sum(wxi))
-    
-    wi[ix] <- wxi
-    #4) Find weighted Lambda by locations
-    locLambdas[[i]] <- t(wi)%*%Lambda #locLambdas[[i]] <- lwi
-    #5) Set Likelihood in cluster i to zero
-    Lik[ix] <- 0
-}
-
-lapply(locLambdas, function(x) summary(x@x))
-lapply(locLambdas, function(x){plotmap(x)})
-
-
-
-
+# #################################################################
+# ############################################
+# #08/14/19
+# ############################################
+#  maxclust <- 10
+#  locLambdas <- vector("list", maxclust)
+# 
+# outExp <- sparsemat%*%Ex #XE
+# outObs <- sparsemat%*%Yx #XY
+# lambdahat <- outObs/outExp
+# Lambda <- as.vector(lambdahat)*sparsemat
+# Lik <- ((outObs/outExp)/(sum(outObs)/sum(outExp)))^outObs
+# outlogLik <- log(Lik)
+# 
+# outlogLik_scaled <- outlogLik - max(outlogLik)
+# Lik <- exp(outlogLik_scaled)
+# 
+# Lambda_dense <- as.matrix(Lambda)
+# Lambda_dense[Lambda_dense == 0] <- 1
+# wi <- likweights(Lik) #tiny weights
+# for (i in 1:maxclust){
+#     #find location wiht largest weight
+#     wi_loc <-t(wi)%*%sparsemat
+#     maxloc <- which.max(as.vector(wi_loc))
+#     message(paste0("Location identified: ",(maxloc)))
+#     #find all potential clusters that overlap that location
+#     locmax <- rep(0,numCenters*Time); locmax[maxloc] <-1; locmax <- matrix(locmax,ncol=1)
+#     pclocmax <- as.vector(t(locmax)%*%t(sparsemat))
+#     #partition weights vector st for pclocmax=1, those weights sum to 1
+#     Lik[which(pclocmax!=0)] <-1
+#     #reweight whats in the cluster so that sums to 1
+#     wi[which(pclocmax!=0)] <- likweights(Lik[which(pclocmax!=0)])
+#     
+#     out <- t(wi)%*%Lambda_dense
+#     locLambdas[[i]] <- out 
+#     #e) Set Lik for overlapping locations to zero
+#     Lik[which(pclocmax!=0)] <-0
+#     #f) Recalculate scaled likelihoods
+#     wi <- likweights(Lik)
+#     
+# }
+# 
+# lapply(locLambdas, function(x) summary(x@x))
+# lapply(locLambdas, function(x) table(x@x))
+# lapply(locLambdas, function(x){plotmap(x, genpdf = FALSE)})
+# vec <- 1:maxclust
+# #lapply(res, function(x) summary(x@x))
+# lapply(1:maxclust, function(i){
+#     name <- paste0("maxclust10_byloc_2019_14_8_nclust", vec[i], ".pdf")
+#     plotmap(res[[i]], pdfname = name)
+# })
