fac27031a23a6cb368cdafa0e973b28ca78fec33
mkamenet3 <maria.kamenetsky@gmail.com>
2022-03-05 12:31:35 -0600

    buckland should be mocked up and minimize info stored so the big comps are just performed once in the calcbounds.cells function

--- R/bounds_new.R
+++ R/bounds_new.R
@@ -1,11 +1,11 @@
 library(matrixcalc)
 ####################################################
-selectuniqRR <- function(uniqRRs){
+selectuniq <- function(uniqRRs, background){
     clusterRR_i <- rep(NA, nrow(uniqRRs))
     flag1 <- sapply(1:nrow(uniqRRs), function(k) length(unique(uniqRRs[k,])))
     non1s <- uniqRRs[which(flag1==2),]
-    clusterRR_i[which(flag1==2)] <- sapply(1:nrow(non1s), function(k) non1s[k,which(non1s[k,]!=1)])
-    clusterRR_i[which(flag1==1)] <- 1    
+    clusterRR_i[which(flag1==2)] <- sapply(1:nrow(non1s), function(k) non1s[k,which(non1s[k,]!= background)])
+    clusterRR_i[which(flag1==1)] <-  background    
     return(unlist(clusterRR_i))
     
 }
@@ -38,7 +38,7 @@ calcbounds <- function(id_ic, IC, res, byloc, Ex, Yx, cellsix=NULL, sparsemat, c
     } 
     thetai_uniq <- sapply(1:nrow(res$Lambda_dense), function(k) unique(res$Lambda_dense[k,]))
     thetai_uniqi <- as.matrix(do.call(rbind, thetai_uniq), ncol=2)
-    thetai <- selectuniqRR(thetai_uniqi)
+    thetai <- selectuniq(thetai_uniqi)
     if(IC=="aic"){
         out <- vector(mode = "list", length = res$selection.aic)
         w <- matrix(res$wtMAT[,1:res$selection.aic], ncol=res$selection.aic)
@@ -108,55 +108,46 @@ calcbounds.cells <- function(id_ic, IC, res, byloc=FALSE, Ex, Yx,w, thetaa,theta
         #Calculate key matrices
         ###########################
         thetaw <- res$Lambda_dense
+        thetaw_uniqvals <- apply(thetaw,1, function(x) unique)
+        thetaw_uniqmat <- data.matrix(as.data.frame(test3c))
+        thetaw_uniq <- selectuniqRR(t(thetaw_uniqmat), 1) #66870
+        
+        
         thetaa <- res$wLambda[res$selection.bic,][cellsix]
         Yx_pc <- t(sparsemat)%*%Yx 
         Yx_pcloc <- as.matrix(sweep(sparsemat, MARGIN=2, Yx_pc@x, `*`)) 
         Yx_pcloc[Yx_pcloc==0] <- Inf
-        var_thetai <- 1/Yx_pcloc
-        
+        if(!is.null(overdisp.est)){
+            var_thetai <- overdisp.est*(1/Yx_pcloc)
+        }else{
+            var_thetai <- 1/Yx_pcloc    
+        }
         
         uniq_thetai <- apply(thetaw,1, function(x) unique(x))
         uniq_thetai_mat<- data.matrix(as.data.frame(uniq_thetai))
-        thetai <- selectuniqRR(t(aa)) #66870
+        thetai_uniq <- selectuniq(t(uniq_thetai_mat)) #66870
+        
+        
+        var_thetai_uniqvals <- apply(var_thetai, MARGIN=2, unique)
+        var_thetai_uniqmat <- data.matrix(as.data.frame(moo2))
+        var_thetai_uniq <- selectuniq(t(var_thetai_uniqmat),0)
+        
+        
+        withinvar <- sapply(1:length(cellsix), function(i) (log(thetaw_uniq) - log(thetaa[i]))^2)
+        
+        
         
         ###########################
         #Calculate Bounds by Method
         ###########################
-        outbuckTlog.theta.time <- system.time(outbuckTlog.theta <- bucklandbounds.cells(thetaa,
-                                                                                        thetai = thetai,
-                                                                                        var_thetai = var_thetai,
-                                                                                        res,
-                                                                                        w=w,
-                                                                                        sparsemat = sparsemat,
-                                                                                        IC=IC,
-                                                                                        transform="log",
-                                                                                        overdisp.est, 
+        outbuckTlog.theta.time <- system.time(outbuckTlog.theta <- bucklandbounds.cells(thetaa, 
+                                                                                        var_thetai_uniq, 
+                                                                                        withinvar, 
+                                                                                        w, 
+                                                                                        sparsemat,
                                                                                         cellsix,
-                                                                                        conf.level))
-                                                                                        
-                                                                                        
-                                                                                        # #Ex,
-                                                                                        # outObs_out,
-                                                                                        # outExp_out,
-                                                                                        # IC=IC,
-                                                                                        # transform="log",
-                                                                                        # tsparsemat=t(sparsemat),
-                                                                                        # overdisp.est, cellsix_out,
-                                                                                        # conf.level))
-        
-        
-        #############################################################################################
-        # outbuckTlog.theta.time <- system.time(outbuckTlog.theta <- bucklandbounds.cells(thetaa,
-        #                                                                                 res,
-        #                                                                                 w=w,
-        #                                                                                 #Ex,
-        #                                                                                 outObs_out,
-        #                                                                                 outExp_out,
-        #                                                                                 IC=IC,
-        #                                                                                 transform="log",
-        #                                                                                 tsparsemat=t(sparsemat),
-        #                                                                                 overdisp.est, cellsix_out,
-        #                                                                                 conf.level))
+                                                                                        critval))
+ 
         message("Buckland bounds finished")
         outba2Tlog.theta.time <- system.time(outba2Tlog.theta <- ba2.cells(thetaa,
                                                                            res,
@@ -207,22 +198,24 @@ calcbounds.cells <- function(id_ic, IC, res, byloc=FALSE, Ex, Yx,w, thetaa,theta
 #'@title bucklandbounds.cells
 #'@description Calculate confidence bounds for stacked cluster relative risk estimates based on Buckland et al. for specific cells (given by \code{cellsix_out}) 
 #'@param thetaa Stacked relative risk estimates for each cell.
-#'@param thetai Unique individual cell relative risk estimates (unstacked).
-#'@param var_thetai Variance for each unique individual cell relative risk estimate.
-#'@param res Resultant object from \code{detectclusters()}.
+#'@param var_thetai_uniq Unique values of variance for each unique individual cell relative risk estimate.
+#'@param withinvar Within estimate variablility (between each unique thetai and the stacked estimate for the cell)
 #'@param w Likelihood-weights for potential clusters.
 #'@param sparsemat Large sparse matrix where columns are potential clusters and rows are space-time locations.
-#'@param IC Information criterion used. Currently available for BIC (QBIC) and AIC (QAIC).
-#'@param transform If a transformation is used. Default is \code{"log"}. Other transformation TBD.
-#'@param overdisp.est Estimate of overdispersion (or underdispersion).
 #'@param cellsix Indices of the cells to calculate bounds for.
 #'@param conf.level Confidence level for the interval. Default is 0.95. 
 #'@return List: lower bound estimate, stacked cluster relative risk estimate, upper bound estimate for each of the cells in \code{cellsix_out}
 #'@export
-
-bucklandbounds.cells <- function(thetaa, thetai, var_thetai, res, w, sparsemat, IC, transform="log", overdisp.est, cellsix, conf.level){
+bucklandbounds.cells <- function(thetaa, var_thetai_uniq, withinvar, w, sparsemat,cellsix, critval){
+    combo <- sapply(1:length(cellsix), function(i) var_thetai_uniq+withinvar[,i])
+    var_est<- sapply(1:length(cellsix), function(i) t(t(sparsemat)*combo[,i]))
+    var_estw <- sapply(1:length(cellsix), function(i) sqrt(var_est[[i]])%*%w)
+    LBa = sapply(1:length(cellsix), function(i) exp(log(thetaa[i])-critval*(var_estw[[i]][cellsix[i]])))
+    UBa = sapply(1:length(cellsix), function(i) exp(log(thetaa[i])+critval*(var_estw[[i]][cellsix[i]])))
     
-    
+    return(list(buckland.LB = LBa,
+                clusterstack= thetaa,
+                buckland.UB = UBa))
 }
 
 
