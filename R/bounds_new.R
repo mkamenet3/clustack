e4a81f533ad7820dd2a0b773a52656bca1b8a8cc
mkamenet3 <maria.kamenetsky@gmail.com>
2022-03-06 14:13:59 -0600

    i think these work, now to condor

--- R/bounds_new.R
+++ R/bounds_new.R
@@ -28,8 +28,9 @@ selectuniq <- function(uniqRRs, background){
 #'\item \code{mataTlog}: Model-average tail area (MATA) intervals based on Turek et al.; natural log-transformed.
 #'
 #'}
-calcbounds <- function(id_ic, IC, res, byloc, Ex, Yx, cellsix=NULL, sparsemat, conf.level=0.95){
+calcbounds <- function(id_ic, IC, res, byloc, Ex, Yx, cellsix=NULL, sparsemat, conf.level=0.95, overdisp.est=NULL){
     #browser()
+    if(is.null(overdisp.est)){overdisp.est<-NULL}
     IC <- tolower(IC)
     if(!is.null(cellsix) & is.null(sparsemat)){
         stop("You must provide the sparsemat when calculating rates for each cell.")
@@ -45,7 +46,7 @@ calcbounds <- function(id_ic, IC, res, byloc, Ex, Yx, cellsix=NULL, sparsemat, c
         w <- matrix(res$wtMAT[,1:res$selection.aic], ncol=res$selection.aic)
         for(i in 1:res$selection.aic){
             thetaa <- sum(thetai*w[,i])
-            out[[i]] <- calcbounds.cells(id_ic, IC, res, byloc, Ex, Yx,as.vector(w[,i]), thetaa,thetai, sparsemat, cellsix, conf.level)
+            out[[i]] <- calcbounds.cells(id_ic, IC, res, byloc, Ex, Yx,as.vector(w[,i]), thetaa,thetai, sparsemat, cellsix, conf.level,overdisp.est)
             
         }
     } else {
@@ -53,7 +54,7 @@ calcbounds <- function(id_ic, IC, res, byloc, Ex, Yx, cellsix=NULL, sparsemat, c
         w <- matrix(res$wtMAT[,1:res$selection.bic], ncol=res$selection.bic)
         for(i in 1:res$selection.bic){
             thetaa <- sum(thetai*w[,i])
-            out[[i]] <-calcbounds.cells(id_ic, IC, res, byloc, Ex, Yx,as.vector(w[,i]), thetaa,thetai, sparsemat, cellsix, conf.level)
+            out[[i]] <-calcbounds.cells(id_ic, IC, res, byloc, Ex, Yx,as.vector(w[,i]), thetaa,thetai, sparsemat, cellsix, conf.level,overdisp.est)
         }
     }
     return(out)
@@ -75,7 +76,7 @@ calcbounds <- function(id_ic, IC, res, byloc, Ex, Yx, cellsix=NULL, sparsemat, c
 #'@param cellsix Indices of the cells to calculate bounds for. 
 #'@param conf.level Confidence level for the interval. Default is 0.95. 
 #'@return Returns large list of confidence bounds and stacked estimates in addition to timings for each of the confidence bounds methods.    
-calcbounds.cells <- function(id_ic, IC, res, byloc=FALSE, Ex, Yx,w, thetaa,thetai, sparsemat, cellsix, conf.level=0.95){
+calcbounds.cells <- function(id_ic, IC, res, byloc=FALSE, Ex, Yx,w, thetaa,thetai, sparsemat, cellsix, conf.level=0.95, overdisp.est){
     #browser()
     if(is.null(conf.level)){conf.level <- 0.95}
     critval <- qnorm(1-(1-conf.level)/2)
@@ -114,7 +115,7 @@ calcbounds.cells <- function(id_ic, IC, res, byloc=FALSE, Ex, Yx,w, thetaa,theta
         #browser()
         thetai_uniqmat <- data.matrix(as.data.frame(thetai_uniqvals))
         thetai_uniq <- selectuniq(t(thetai_uniqmat), 1) #66870
-        
+        message("Extracted unique thetai's")
         
         thetaa <- res$wLambda[res$selection.bic,][cellsix]
         Yx_pcloc <- t(sparsemat)%*%Yx 
@@ -131,10 +132,12 @@ calcbounds.cells <- function(id_ic, IC, res, byloc=FALSE, Ex, Yx,w, thetaa,theta
         # var_thetai_uniqmat <- data.matrix(as.data.frame(var_thetai_uniqvals))
         # var_thetai_uniq <- selectuniq(t(var_thetai_uniqmat),0)
         
-        
+        #browser()
         withinvar <- sapply(1:length(cellsix), function(i) (log(thetai_uniq) - log(thetaa[i])))
         ses <- sapply(1:length(cellsix), function(i) (se_thetai)^2 + (withinvar[,i])^2)
-        var_est<- sapply(1:length(cellsix), function(i) t(t(sparsemat)*as.vector(ses[[i]])))
+        #system.time(var_est<- sapply(1:length(cellsix), function(i) t(t(sparsemat)*as.vector(ses[[i]]))))
+        var_est<- sapply(1:length(cellsix), function(i) sparsemat %*% Diagonal(length(as.vector(ses[[i]])),
+                                                                                         as.vector(ses[[i]])))
         #browser()
         message("Finished calculating SEs")
         
@@ -158,7 +161,8 @@ calcbounds.cells <- function(id_ic, IC, res, byloc=FALSE, Ex, Yx,w, thetaa,theta
                                                                            critval))
         
         message("Burnham & Anderson bounds finished")
-        outmataTlog.theta.time <- system.time(outmataTlog.theta <- matabounds_log.cells(ses, thetai, thetaa, cellsix,alpha2))
+        outmataTlog.theta.time <- system.time(outmataTlog.theta <- matabounds_log.cells(var_est, thetai, thetaa, w,
+                                                                                        cellsix,alpha2))
         message("MATA bounds finished")
         
         ###########################
@@ -217,29 +221,33 @@ ba2.cells <- function(thetaa, var_est, w, sparsemat,cellsix, critval){
 }
 
 #########MATA Bounds
-mata_ZLB <- function(theta, sesi,thetaii){
+mata_ZLB <- function(theta, sesi,thetaii,w){
     resLB <-sum(w*(1-pnorm((log(thetaii)-theta)/(sesi+(log(thetaii)-theta==0)))))
     
 }
 
-mata_ZUB <- function(theta,sesi, thetaii){
-    1-mata_ZLB(theta,sesi, thetaii)
+mata_ZUB <- function(theta,sesi, thetaii,w){
+    1-mata_ZLB(theta,sesi, thetaii,w)
 }
-f_LB<-function(theta,sesi,thetaii, alpha2){
-    mata_ZLB(theta, sesi,thetaii)-alpha2  
+f_LB<-function(theta,sesi,thetaii,w, alpha2){
+    mata_ZLB(theta, sesi,thetaii,w)-alpha2  
 } 
-f_UB<-function(theta,sesi,thetaii,alpha2){
-    mata_ZUB(theta,sesi, thetaii)-alpha2  
+f_UB<-function(theta,sesi,thetaii,w,alpha2){
+    mata_ZUB(theta,sesi, thetaii,w)-alpha2  
 } 
 
-matabounds_log.cells <- function(ses,thetai, thetaa, cellsix,alpha2){
+matabounds_log.cells <- function(var_est,thetai, thetaa,w, cellsix,alpha2){
+    #browser()
+    se_est <- lapply(1:length(cellsix), function(i) sqrt(var_est[[i]]))
     LBa <- sapply(1:length(cellsix), function(i) exp(uniroot(f_LB,c(-3,3),
-                                                             sesi=ses[cellsix[i],],
+                                                             sesi=se_est[[i]][cellsix[i],],
                                                              thetaii=thetai[,cellsix[i]],
+                                                             w=w,
                                                              alpha2=alpha2)$root))
     UBa <- sapply(1:length(cellsix), function(i) exp(uniroot(f_UB,c(-3,3),
-                                                             sesi=ses[cellsix[i],],
+                                                             sesi=se_est[[i]][cellsix[i],],
                                                              thetaii=thetai[,cellsix[i]],
+                                                             w=w,
                                                              alpha2=alpha2)$root))
     return(list(mata.LB = LBa,
             clusterstack= thetaa,
